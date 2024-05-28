97f07a0f7269183753d9cfb77ef4c8b839831b04
joliebao <142067108+joliebao@users.noreply.github.com>
2024-05-28 06:44:43 -0400

    girl forward
    I forgot to upload it from yesterday!

--- girl.py
+++ girl.py
@@ -6,7 +6,7 @@ class Girl:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("pixilart-drawing.png")
+        self.image = pygame.image.load("girl backward.png")
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
         scale_size = (self.image_size[0] * 5, self.image_size[1] * 5)
@@ -14,12 +14,17 @@ class Girl:
         self.image_size = self.image.get_size()
         self.delta = 8
         self.current_direction = "left"
+        self.current_direction = "backward"
 
     def move_direction(self, direction):
         if self.current_direction == "right" and direction == "left":
+            self.image = pygame.image.load("girl backward.png")
             self.image = pygame.transform.flip(self.image, True, False)
         elif self.current_direction == "left" and direction == "right":
+            self.image = pygame.image.load("girl backward.png")
             self.image = pygame.transform.flip(self.image, True, False)
+        elif self.current_face == "forward" and direction == "down":
+            self.image = pygame.image.load("girl forward.png")
         if direction == "right":
             self.current_direction = "right"
             self.x = self.x + self.delta
